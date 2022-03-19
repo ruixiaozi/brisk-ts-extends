0aaf0b5a3bca6208b524de0c9cafb8b50f6968de
ruixiaozi <admin@ruixiaozi.com>
2022-03-19 18:50:01 +0800

    feat(1.0.5): 更新

---
+++ is.d.ts
@@ -0,0 +1,2 @@
+export declare function configPath(path: string): void;
+export declare function is<T>(target: any, typeName: string): target is T;
--- src/is/is.ts
+++ src/is/is.ts
@@ -3,42 +3,46 @@ import * as fs from 'fs';
 
 let config: any = {};
 
-export function configPath(path: string){
-  if(!fs.existsSync(path)) {
+
+export function configPath(path: string) {
+  if (!fs.existsSync(path)) {
     throw new Error('brisk-ts-extends: [is] module config path is not exists');
   }
   config = require(path);
-  if(typeof config !== 'object'){
+  if (typeof config !== 'object') {
     throw new Error('brisk-ts-extends: [is] module config is error');
   }
 }
 
 export function is<T>(target: any, typeName: string): target is T {
   // 没找到对应的类型定义
-  if(!config[typeName] || typeof target !== 'object') {
+  if (!config[typeName] || typeof target !== 'object') {
     return false;
   }
 
   // 类型定义不是对象，错误的格式
-  if(typeof config[typeName] !== 'object'){
+  if (typeof config[typeName] !== 'object') {
     throw new Error('brisk-ts-extends: [is] module config is error');
   }
 
-  for(let [key, value] of Object.entries(config[typeName])){
-    if(!Array.isArray(value)){
+  for (let [key, value] of Object.entries(config[typeName])) {
+    if (!Array.isArray(value)) {
       throw new Error('brisk-ts-extends: [is] module config is error');
     }
     let isRight = false;
-    for(let typeStr of value){
-      if(typeof typeStr !== 'string'){
+    for (let typeStr of value) {
+      if (typeof typeStr !== 'string') {
         throw new Error('brisk-ts-extends: [is] module config is error');
       }
-      isRight ||=  typeof target[key] === typeStr;
+      isRight ||= typeof target[key] === typeStr;
     }
-    if(!isRight){
+    if (!isRight) {
       return false;
     }
   }
 
   return true;
 }
+
+
+
